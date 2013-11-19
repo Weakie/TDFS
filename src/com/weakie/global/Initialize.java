c8c3db66ea1dc4c796b29122be50d5f1eac22c2c
孙渊汇 <syhkiller@163.com>
2013-11-19 12:13:22 +0800

    complete the listener

--- src/com/weakie/global/Initialize.java
+++ src/com/weakie/global/Initialize.java
@@ -2,24 +2,28 @@ package com.weakie.global;
 
 import java.io.IOException;
 import java.net.ServerSocket;
-import java.net.Socket;
 
 public class Initialize {
 
 	private ServerSocket listen;
 	private int listenPort;
 	
+	public static Initialize instance = new Initialize();
 	private Initialize(){};
 	
+	public static Initialize getInstance(){
+		return instance;
+	}
 	
 	public void startup() throws IOException{
 		listen = new ServerSocket(listenPort);
-		Socket socket = listen.accept();
-		
+		//Socket socket = listen.accept();
+		System.out.println("startup ");
 	}
 	
 	public void shutdown() throws IOException{
 		listen.close();
+		System.out.println("shutdown ");
 	}
 	
 	public int getListenPort(){
--- src/com/weakie/global/Listener.java
+++ src/com/weakie/global/Listener.java
@@ -1,5 +1,7 @@
 package com.weakie.global;
 
+import java.io.IOException;
+
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
 
@@ -13,7 +15,30 @@ public final class Listener implements ServletContextListener {
      * Default constructor. 
      */
     public Listener() {
-        // TODO Auto-generated constructor stub
+    	System.out.println("listener new ");
     }
+
+	public void contextDestroyed(ServletContextEvent arg0) {
+		String name = arg0.getServletContext().getServletContextName();
+		System.out.println("destroy "+name);
+		Initialize instance = Initialize.getInstance();
+		try {
+			instance.shutdown();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void contextInitialized(ServletContextEvent arg0) {
+		String name = arg0.getServletContext().getServletContextName();
+		System.out.println("initial "+name);
+		Initialize instance = Initialize.getInstance();
+		instance.setListenPort(10230);
+		try {
+			instance.startup();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 	
 }
