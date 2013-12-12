86014f249255f8ca88ccbeadff3f7ae36848cca8
Weakie <weakielin@gmail.com>
2013-12-12 16:43:00 +0800

    file servlet

---
+++ WebContent/jsp/filelist.jsp
@@ -0,0 +1,24 @@
+<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
+    pageEncoding="ISO-8859-1"%>
+<%@ page import="java.io.*" %>
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
+<title>Insert title here</title>
+</head>
+<body>
+<% 
+	File[] files = (File[])request.getAttribute("files");
+	File parent = (File)request.getAttribute("file");
+%>
+	<%=parent.getAbsolutePath() %>
+<%
+	for(File file : files){
+%>
+		<%=file.getName()%><%=(file.isDirectory()?File.separator:"")%><br>
+<%
+	}
+%>
+</body>
+</html>
---
+++ src/com/weakie/web/FileListServlet.java
@@ -0,0 +1,52 @@
+package com.weakie.web;
+
+import java.io.File;
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.lang3.StringUtils;
+
+import com.weakie.util.log.LogUtil;
+
+/**
+ * Servlet implementation class FileListServlet
+ */
+public class FileListServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public FileListServlet() {
+        super();
+        LogUtil.info("Servlet init.");
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String dir = request.getParameter("baseDir");
+		LogUtil.info("new request:"+dir);
+		if(StringUtils.isEmpty(dir)){
+			dir = getServletContext().getInitParameter("baseDir");
+		}
+		File file = new File(dir);
+		File[] files = file.listFiles();
+		request.setAttribute("file", file);
+		request.setAttribute("files", files);
+		request.getRequestDispatcher("jsp/filelist.jsp").forward(request, response);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request,response);
+	}
+
+}
